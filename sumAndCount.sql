89fddc53d8e4de3cbfa0c01ce7c37ab0699d6b52
AlFalah <taiebalfalah@gmail.com>
2019-06-11 15:58:27 +0200

    Update Sum andCount

--- sumAndCount.sql
+++ sumAndCount.sql
@@ -1,20 +1,38 @@
--- 1. Show the total population of the world.
-
--- 2. List all the continents - just once each.
-
--- 3. Give the total GDP of Africa.
-
--- 4. How many countries have an area of at least 1000000.
-
--- 5. What is the total population of ('Estonia', 'Latvia', 'Lithuania').
-
--- 6. For each continent show the continent and number of countries.
-
--- 7. For each continent show the continent and number of countries
--- with populations of at least 10 million.
+-- 1. Modify it to show the matchid and player name for all goals scored by Germany. To identify German players, check for: teamid = 'GER'
 
 
--- 8. List the continents that have a total population of at least
--- 100 million.
+-- 2. Show id, stadium, team1, team2 for just game 1012
 
 
+-- 3. Modify it to show the player, teamid, stadium and mdate and for every German goal.
+
+
+
+-- 4. Show the team1, team2 and player for every goal scored by a player called Mario player LIKE 'Mario%'
+
+-- 5. Show player, teamid, coach, gtime for all goals scored in the first 10 minutes gtime<=10
+
+
+-- 6. List the the dates of the matches and the name of the team in which 'Fernando Santos' was the team1 coach.
+
+
+
+-- 7. List the player for every goal scored in a game where the stadium was 'National Stadium, Warsaw'
+
+
+-- 8. Instead show the name of all players who scored a goal against Germany.
+
+	
+-- 9. Show teamname and the total number of goals scored.
+
+
+
+-- 10. Show the stadium and the number of goals scored in each stadium.
+
+
+-- 11. For every match involving 'POL', show the matchid, date and the number of goals scored.
+
+-- 12. For every match where 'GER' scored, show matchid, match date and the number of goals scored by 'GER'
+
+
+-- 13. List every match with the goals scored by each team as shown.
