23b3203d05d306c30a2995a7f4115f50df122d2f
AlFalah <taiebalfalah@gmail.com>
2019-06-17 10:00:37 +0200

    List all of the Star Trek movies

--- moreJoin.sql
+++ moreJoin.sql
@@ -1,13 +1,18 @@
 -- 1. List the films where the yr is 1962 [Show id, title]
-
 SELECT id, title
  FROM movie
  WHERE yr=1962
+
 -- 2. Give year of 'Citizen Kane'.
 
+SELECT yr
+ FROM movie
+ WHERE title='Citizen Kane'
 
 -- 3. List all of the Star Trek movies, include the id, title and yr (all of these movies include the words Star Trek in the title). Order results by year.
-
+SELECT id, title, yr
+ FROM movie
+ WHERE title LIKE '%Star Trek' OR title LIKE 'Star Trek%'
 
 -- 4. What are the titles of the films with id 11768, 11955, 21191
 
