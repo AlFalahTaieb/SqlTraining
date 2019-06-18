406d9351adb78ccb6cf5fa34cb8d026fb8e3956e
AlFalah <taiebalfalah@gmail.com>
2019-06-18 16:06:29 +0200

    films released in the year 1978

--- moreJoin.sql
+++ moreJoin.sql
@@ -92,7 +92,11 @@ ORDER BY actor.name
 -- 15. List the films released in the year 1978 ordered by the number of actors in the cast, then by title.
 
 
-
+SELECT title, count(actorid)
+FROM  movie JOIN casting ON id=casting.movieid
+WHERE yr = 1978
+GROUP by movie.title
+ORDER BY count(actorid) DESC , title
 -- 16. List all the people who have worked with 'Art Garfunkel'.
 
 
