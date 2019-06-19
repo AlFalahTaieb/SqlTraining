9951c206f8d61f0b12cb6f18406f21bcfca41f80
AlFalah <taiebalfalah@gmail.com>
2019-06-19 23:02:58 +0200

    LEFT JOIN

--- null.sql
+++ null.sql
@@ -7,7 +7,9 @@ SELECT teacher.name, dept.name
            ON (teacher.dept=dept.id)
 -- 3. Use a different JOIN so that all teachers are listed.
 
-
+SELECT teacher.name ,dept.name
+FROM teacher LEFT JOIN dept
+           ON (teacher.dept=dept.id)
 
 -- 4. Use a different JOIN so that all departments are listed.
 
