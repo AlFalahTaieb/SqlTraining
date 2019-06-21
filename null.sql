41f263202de6a0a1ec9e016d230bb0da4b5d1905
AlFalah <taiebalfalah@gmail.com>
2019-06-21 11:21:59 +0200

    Using Case

--- null.sql
+++ null.sql
@@ -40,7 +40,12 @@ FROM teacher RIGHT JOIN dept
 GROUP BY dept.name
 
 -- 9. Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2 and 'Art' otherwise.
-
+SELECT teacher.name
+      ,CASE WHEN dept=1 or dept=2           
+            THEN 'Sci'
+            ELSE 'Art'
+       END
+  FROM teacher
 
 
 
