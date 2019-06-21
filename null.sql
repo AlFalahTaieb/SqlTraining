0747dc07d07611441d0dec5f454007b55904fd46
AlFalah <taiebalfalah@gmail.com>
2019-06-21 11:17:44 +0200

    Use COUNT and GROUP BY dept.name to show each department and the number of staff.

--- null.sql
+++ null.sql
@@ -34,7 +34,10 @@ From teacher
 
 -- 8. Use COUNT and GROUP BY dept.name to show each department and the number of staff. Use a RIGHT JOIN to ensure that the Engineering department is listed.
 
-
+SELECT dept.name, COUNT(teacher.name) 
+FROM teacher RIGHT JOIN dept
+           ON (teacher.dept=dept.id)
+GROUP BY dept.name
 
 -- 9. Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2 and 'Art' otherwise.
 
