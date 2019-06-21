753cae37b17952eb8874e7c660b295aead274ffd
AlFalah <taiebalfalah@gmail.com>
2019-06-21 11:41:20 +0200

    Last UseCase

--- null.sql
+++ null.sql
@@ -52,7 +52,12 @@ SELECT teacher.name
 -- 10. Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2, show 'Art' if the teacher's dept is 3 and 'None' otherwise
 
 
-
+SELECT teacher.name
+      ,CASE WHEN dept=1 or dept=2 THEN 'Sci' 
+WHEN  dept=3  THEN 'Art' 
+ELSE   'None'
+       END
+  FROM teacher
 
 
 
