608acfafe73f293391774fd2b3face2df4a7796d
AlFalah <taiebalfalah@gmail.com>
2019-06-21 11:11:55 +0200

    Use the COALESCE function and a LEFT JOIN to print the teacher name and department name

--- null.sql
+++ null.sql
@@ -23,7 +23,9 @@ SELECT teacher.name
 FROM teacher 
 
 -- 6. Use the COALESCE function and a LEFT JOIN to print the teacher name and department name. Use the string 'None' where there is no department.
-
+SELECT teacher.name,COALESCE(dept.name,'None') AS Name
+FROM teacher LEFT JOIN dept
+           ON (teacher.dept=dept.id)
 
 -- 7. Use COUNT to show the number of teachers and the number of mobile phones.
 
