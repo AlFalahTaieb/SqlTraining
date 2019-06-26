3780d4d6200b4459881c079c627151cc8a2dfbb8
AlFalah <taiebalfalah@gmail.com>
2019-06-27 00:35:47 +0200

    Give the id and the name for the stops on the '4' 'LRT' service.

--- selfJoin.sql
+++ selfJoin.sql
@@ -7,7 +7,9 @@ Select id
 From stops
 where name = 'Craiglockhart' 
 -- 3. Give the id and the name for the stops on the '4' 'LRT' service.
-
+Select id, name
+From stops JOIN route on id=stop
+where num=4 AND company = 'LRT'
 --4. he query shown gives the number of routes that visit either London Road (149) or Craiglockhart (53). Run the query and notice the two services that link these stops have a count of 2. Add a HAVING clause to restrict the output to these two routes.
 
 
